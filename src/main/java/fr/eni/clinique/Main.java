10a4cc57b65be9b30ffb6209d97308da1df882c2
Reliquat <raphael.taque@gmail.com>
2018-03-09 10:57:28 +0100

    accueil

--- src/main/java/fr/eni/clinique/Main.java
+++ src/main/java/fr/eni/clinique/Main.java
@@ -2,10 +2,16 @@ package fr.eni.clinique;
 
 import javax.swing.SwingUtilities;
 
+import fr.eni.clinique.bo.Personnel;
+import fr.eni.clinique.common.AppConstants;
 import fr.eni.clinique.common.exception.TechnicalException;
-import fr.eni.clinique.ihm.controller.AgendaController;
-import fr.eni.clinique.ihm.model.AgendaModel;
-import fr.eni.clinique.ihm.screen.agenda.AgendaScreen;
+import fr.eni.clinique.ihm.controller.AccueilController;
+import fr.eni.clinique.ihm.controller.LoginController;
+import fr.eni.clinique.ihm.controller.PersonnelController;
+import fr.eni.clinique.ihm.model.LoginModel;
+import fr.eni.clinique.ihm.model.PersonnelModel;
+import fr.eni.clinique.ihm.screen.admin.AdminScreen;
+import fr.eni.clinique.ihm.screen.login.LoginScreen;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,37 +20,41 @@ public class Main {
 	        SwingUtilities.invokeLater(new Runnable() {
 	            @Override
 	            public void run() {
-	            	
-	            	/*LoginScreen.main(args);
-	            	
-	            	
+
 	            	LoginModel loginModel = new LoginModel();
 	            	LoginController loginController = new LoginController(loginModel);
-	            	
-	            	LoginScreen logScreen = new LoginScreen(AppConstants.APP_NAME);
-	                
-	                logScreen.setVisible(true);
-	                logScreen.setLocationRelativeTo(null);
-	                logScreen.setActionListener(loginController);
-	                
-	                ScreenClient custScreen = new ScreenClient(AppConstants.APP_NAME);
-	                
-	                custScreen.main(args);*/
-	            	
-	            	AgendaScreen agendaScreen = new AgendaScreen();
-	            	
-	            	AgendaModel agendaModel = new AgendaModel();
-	            	AgendaController agendaController = new AgendaController(agendaModel);
-	            	
-	            	agendaScreen.setActionListener(agendaController);
-	            	
-	            	agendaScreen.frmAgenda.setVisible(true);
-	            	agendaScreen.frmAgenda.setLocationRelativeTo(null);
-	            	
+	            	LoginScreen loginScreen = new LoginScreen(AppConstants.APP_NAME);
+
+	                loginScreen.setVisible(true);
+	                loginScreen.setLocationRelativeTo(null);
+	                loginScreen.setActionListener(loginController);
 	            }
 	        });
+	        
 	    } catch (Exception e) {
 	        throw new TechnicalException("Erreur Technique", e);
 	    }
 	}
+	
+	public static void launchApp(Personnel personnel) {
+		
+		new AccueilController();
+    	
+		switch (personnel.getRole()) {
+
+			case "sec":
+                break;
+                
+			case "vet":
+				break;
+				
+			case "adm":
+				AdminScreen adminScreen = new AdminScreen();
+				PersonnelModel personnelModel = new PersonnelModel();
+				PersonnelController personnelController = new PersonnelController(personnelModel);
+				adminScreen.setActionListener(personnelController);
+				adminScreen.frmGestionDuPersonnel.setVisible(true);
+				break;
+		}
+    }
 }
